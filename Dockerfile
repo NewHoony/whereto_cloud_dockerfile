f87f433165a2a5177e962f76efd7b75d4d5ac878
NewHoony <shinpingu1@naver.com>
2023-03-27 03:01:39 +0000

    docer image

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM python:3.8.0
+
+ENV PYTHONUNBUFFERED 1
+
+RUN apt-get -y update
+
+RUN mkdir -p /home/ec2-user/whereto
+
+ADD . /home/ec2-user/whereto
+
+WORKDIR /home/ec2-user/whereto
+
+RUN pip install --upgrade pip
+RUN pip install -r requirements.txt
+RUN python3 manage.py migrate
+RUN python3 manage.py migrate --run-syncdb
---
+++ config/dbrouter.py
@@ -0,0 +1,19 @@
+import random
+from django.db import transaction
+
+class MultiDBRouter(object):
+    def db_for_read(self, model, **hints):
+        @staticmethod
+        def db_for_read(model, **hints):
+            transaction.get_autocommit()
+            conn = transaction.get_connection('default')
+            if conn.in_atomic_block:
+                return 'default'
+        databases = ['default','readonly']
+        return random.choice(databases)
+
+    def db_for_write(self, model, **hints):
+        return "default"
+
+    def allow_relation(self, obj1, obj2, **hints):
+        return True
